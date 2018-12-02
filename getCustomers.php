3c880b01ab679bb2065b5d4f5381184292bc66c1
JacobHuang <jacobhuang660@gmail.com>
2018-12-02 14:05:43 -0500

    get customers

---
+++ getCustomers.php
@@ -0,0 +1,16 @@
+<<?php
+$query = "SELECT LastName FROM Customers ORDER BY LastName ASC;";
+$result = mysqli_query($connection,$query);
+if (!$result) {
+   die("databases query failed.");
+}
+echo "<ol>";
+while ($row = mysqli_fetch_assoc($result)) {
+  echo "<option value='";
+  echo $row["LastName"]"'>";
+  echo "</option>";
+}
+mysqli_free_result($result);
+
+
+?>
--- home.php
+++ home.php
@@ -3,21 +3,15 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Dr. Western's Vet Clinic</title>
+<title>CS3319 Assignment3</title>
 </head>
 <body>
 <?php
 include 'connectdb.php';
 ?>
-<h1>Welcome to the Western Vet Clinic</h1>
-<h2>Pets we look after</h2>
 <?php
-include 'getdata.php';
+include 'getCustomers.php';
 ?>
-<ol>
-<li>Dogs</li>
-<li>Cats</li>
-<li>Birds</li>
-</ol>
+
 </body>
 </html>
