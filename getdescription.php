a106c12627d94fe142172445544415b13c1c536f
JacobHuang <jacobhuang660@gmail.com>
2018-12-02 23:53:59 -0500

    asdd

--- getdescription.php
+++ getdescription.php
@@ -1,5 +1,5 @@
 <?php
-$whichCus = $_POST["pickaCustomer"]; //get selected museum value from the form
+//$whichCus = $_POST["pickaCustomer"]; //get selected museum value from the form
 echo $whichCus;
 $query = "SELECT description FROM Products WHERE ProductID IN (SELECT ProductID FROM Purchase WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE LastName = ".Bob."));"; //fill in with correct query
 //$query = "SELECT LastName FROM Customers ORDER BY LastName ASC;";
@@ -8,12 +8,11 @@ $result = mysqli_query($connection, $query);
  if (!$result) {
  die("databases query on art pieces failed. ");
  }
- echo $result;
- // echo "<ul>"; //put the artwork in an unordered bulleted list
- // while ($row = mysqli_fetch_assoc($result)) {
- //   echo "Bought";
- //   echo "<li>" .$row["description"]. "</li>";
- // }
- // echo "</ul>"; //end the bulleted list
+ echo "<ul>"; //put the artwork in an unordered bulleted list
+ while ($row = mysqli_fetch_assoc($result)) {
+   echo "Bought";
+   echo "<li>" .$row["description"]. "</li>";
+ }
+ echo "</ul>"; //end the bulleted list
  mysqli_free_result($result);
  ?>
