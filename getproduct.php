f7c1722ca402459de999de533662c63a4011092c
JacobHuang <jacobhuang660@gmail.com>
2018-12-03 12:04:24 -0500

    asdd

---
+++ getproduct.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+include 'connectdb.php';
+?>
+<h1>Here are the products:</h1>
+<ol>
+<?php
+   $orderByPrice= $_POST["price"];
+   $orderByDescription= $_POST["description"];
+   $query1 = 'SELECT * FROM Products ORDER BY Cost '.$orederByPrice.'';
+   $query2 = 'SELECT * FROM Products ORDER BY Description '.$orederByDescription.'';
+   //$query = 'SELECT * FROM owner, pet WHERE pet.ownerid=owner.ownerid AND pet.ownerid="' . $whichOwner . '"';
+   $result=mysqli_query($connection,$query1);
+    if (!$result) {
+         die("database query2 failed.");
+     }
+    while ($row=mysqli_fetch_assoc($result)) {
+        echo $row["ProductID"]. " " .$row["Description"]. " " .$row["Cost"]. " " .$row["Quantity"]. " ";
+        echo "<br>";
+     }
+     mysqli_free_result($result);
+
+
+?>
+</ol>
+<?php
+   mysqli_close($connection);
+?>
+</body>
+</html>
--- home.php
+++ home.php
@@ -9,7 +9,7 @@
 <?php
 include 'connectdb.php';
 ?>
-<h1>Q1:</h1>
+<h4>Q1:</h4>
 Select a Customers:
 <form action="getdescription.php" method="post">
 <?php
@@ -18,7 +18,20 @@ include 'getCustomers.php';
 <input type="submit" value="submit">
 </form>
 <hr>
-
+<h4>Q2:</h4>
+List Products
+<form action="getproduct.php">
+  Oreder by Price:<br>
+  <input type="radio" name="price" value="ASC"> Ascending<br>
+  <input type="radio" name="price" value="DESC"> Descending<br>
+  <input type="submit" value="Submit">
+</form>
+<form action="getproduct.php">
+  Oreder by description:<br>
+  <input type="radio" name="description" value="ASC"> Ascending<br>
+  <input type="radio" name="description" value="DESC"> Descending<br>
+  <input type="submit" value="Submit">
+</form>
 <hr>
 
 
