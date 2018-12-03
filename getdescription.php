78d954f7af3a0536eef05c604deaa13694d3cb85
JacobHuang <jacobhuang660@gmail.com>
2018-12-03 02:18:53 -0500

    asdd

--- getdescription.php
+++ getdescription.php
@@ -9,7 +9,7 @@
     $whichCus = $_POST["value"]; //get selected museum value from the form
     echo $whichCus;
     //$query = "SELECT description FROM Products WHERE ProductID IN (SELECT ProductID FROM Purchase WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE LastName = ".$whichCus."));"; //fill in with correct query
-    $query = $query = "SELECT * FROM products, purchases, customers WHERE products.product_id=purchases.product_id AND customers.customer_id = purchases.customer_id AND customers.customer_id = $whichCus";
+    $query = "SELECT * FROM Products, Purchase, Customers WHERE Products.ProductID=Purchase.ProductID AND Customers.customerID = Purchase.CustomerID AND Customers.CustomerID = $whichCus";
     echo $query;
     $result = mysqli_query($connection, $query);
      if (!$result) {
