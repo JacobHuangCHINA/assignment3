f071ebec0d5a246198e2d4f837fa80519436f4a9
JacobHuang <jacobhuang660@gmail.com>
2018-12-03 10:42:40 -0500

    asdd

--- getdescription.php
+++ getdescription.php
@@ -1,32 +1,31 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
 include 'connectdb.php';
 ?>
+<h1>Here are what customer purchase</h1>
 <ol>
-    <?php
-    $whichCus = $_POST["value"]; //get selected museum value from the form
-    echo $whichCus;
-    //$query = "SELECT description FROM Products WHERE ProductID IN (SELECT ProductID FROM Purchase WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE LastName = ".$whichCus."));"; //fill in with correct query
-    $query = "SELECT * FROM Products, Purchase, Customers WHERE Products.ProductID=Purchase.ProductID AND Customers.customerID = Purchase.CustomerID AND Customers.CustomerID = $whichCus";
-    echo $query;
-    $result = mysqli_query($connection, $query);
-     if (!$result) {
-     die("databases query on art pieces failed. ");
-   }
-     while ($row = mysqli_fetch_assoc($result)) {
-       echo "<li>" .$row["description"]. "</li>";
+<?php
+   $whichOwner= $_POST["Customers"];
+   $query = 'SELECT * FROM Products, Purchase, Customers WHERE Products.ProductID=Purchase.ProductID AND Customers.customerID = Purchase.CustomerID AND Customers.CustomerID = "' . $whichOwner . '"';
+   //$query = 'SELECT * FROM owner, pet WHERE pet.ownerid=owner.ownerid AND pet.ownerid="' . $whichOwner . '"';
+   $result=mysqli_query($connection,$query);
+    if (!$result) {
+         die("database query2 failed.");
+     }
+    while ($row=mysqli_fetch_assoc($result)) {
+        echo '<li>';
+        echo $row["description"];
      }
      mysqli_free_result($result);
-     ?>
-   </ol>
-   <?php
+?>
+</ol>
+<?php
    mysqli_close($connection);
-   ?>
-  </body>
+?>
+</body>
 </html>
