7a8079a9cc5f626fd91067647a22846a4b42c186
JacobHuang <jacobhuang660@gmail.com>
2018-12-03 10:51:52 -0500

    asdd

--- getdescription.php
+++ getdescription.php
@@ -10,8 +10,8 @@ include 'connectdb.php';
 <h1>Here are what customer purchase</h1>
 <ol>
 <?php
-   $whichOwner= $_POST["value"];
-   $query = 'SELECT * FROM Products, Purchase, Customers WHERE Products.ProductID=Purchase.ProductID AND Customers.customerID = Purchase.CustomerID AND Customers.CustomerID = "' . $whichOwner . '"';
+   $whichOwner= $_POST["Customers"];
+   $query = 'SELECT description FROM Products, Purchase, Customers WHERE Products.ProductID=Purchase.ProductID AND Customers.customerID = Purchase.CustomerID AND Customers.CustomerID = "' . $whichOwner . '"';
    //$query = 'SELECT * FROM owner, pet WHERE pet.ownerid=owner.ownerid AND pet.ownerid="' . $whichOwner . '"';
    $result=mysqli_query($connection,$query);
     if (!$result) {
