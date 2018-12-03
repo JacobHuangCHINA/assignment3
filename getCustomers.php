a832034e3674e6792dbe15a51f838b79365626e4
JacobHuang <jacobhuang660@gmail.com>
2018-12-03 00:57:51 -0500

    asdd

--- getCustomers.php
+++ getCustomers.php
@@ -6,10 +6,10 @@ if (!$result) {
 }
 echo "<ol>";
 while ($row = mysqli_fetch_assoc($result)) {
-  echo "<tr><td> CustomerID </td><td> LastName FirstName City PhoneNum AgentID </td>";
-  echo "<input value='";
+  echo "<tr><td>CustomerID</td><td>LastName FirstName</td><td>City</td><td>PhoneNum</td><td>AgentID</td></tr>";
+  echo "<input type='radio' value='";
   echo $row["CustomerID"]."'>";
-  echo "<tr><td>" .$row["CustomerID"]. "</td><td>" .$row["LastName"]. " " .$row["FirstName"]. " " .$row["City"]. " ".$row["PhoneNum"]. " " .$row["AgentID"]. "</td>";
+  echo "<tr><td>" .$row["CustomerID"]. "</td><td>" .$row["LastName"]. .$row["FirstName"]. "</td><td>" .$row["City"]. "</td><td>".$row["PhoneNum"]. "</td><td>" .$row["AgentID"]. "</td></tr>";
   echo "<br>";
 }
 mysqli_free_result($result);
