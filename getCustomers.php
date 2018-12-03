d8f8ef865cc8bfb0023974ca18d362c318c28d1e
JacobHuang <jacobhuang660@gmail.com>
2018-12-02 20:06:17 -0500

    asdd

--- getCustomers.php
+++ getCustomers.php
@@ -7,7 +7,7 @@ if (!$result) {
 echo "<ol>";
 while ($row = mysqli_fetch_assoc($result)) {
   echo "<option value='";
-  echo $row["CustomerID"]."'>";
+  echo $row["LastName"]."'>";
   echo $row["LastName"];
   echo "</option>";
 }
