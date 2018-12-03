ca56d252d5b472cacf701abee23bc31935e8e62e
JacobHuang <jacobhuang660@gmail.com>
2018-12-03 00:08:52 -0500

    asdd

--- getdescription.php
+++ getdescription.php
@@ -2,7 +2,7 @@
 $whichCus = $_POST["pickaCustomer"]; //get selected museum value from the form
 echo $whichCus;
 //$query = "SELECT description FROM Products WHERE ProductID IN (SELECT ProductID FROM Purchase WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE LastName = ".$whichCus."));"; //fill in with correct query
-$query = "SELECT LastName FROM Customers ORDER BY LastName ASC;";
+$query = $query = "SELECT * FROM products, purchases, customers WHERE products.product_id=purchases.product_id AND customers.customer_id = purchases.customer_id AND customers.customer_id = $whichCus";
 echo $query;
 $result = mysqli_query($connection, $query);
  if (!$result) {
@@ -11,7 +11,7 @@ $result = mysqli_query($connection, $query);
  echo "<ul>"; //put the artwork in an unordered bulleted list
  while ($row = mysqli_fetch_assoc($result)) {
    echo "Bought";
-   echo "<li>" .$row["LastName"]. "</li>";
+   echo "<li>" .$row["description"]. "</li>";
  }
  echo "</ul>"; //end the bulleted list
  mysqli_free_result($result);
