18110c04ad8a61f625a5998ff44b06d3f720ba6e
JacobHuang <jacobhuang660@gmail.com>
2018-12-03 13:37:42 -0500

    asdd

--- getproduct.php
+++ getproduct.php
@@ -12,18 +12,18 @@ include 'connectdb.php';
 <?php
    $orderByPrice= $_POST["price"];
    $orderByDescription= $_POST["description"];
-   $query1 = 'SELECT * FROM Products ORDER BY Cost ASE';//"'.$orederByPrice.'"'
+   $query1 = 'SELECT * FROM Products ORDER BY Cost '.$orederByPrice.'';
    $query2 = 'SELECT * FROM Products ORDER BY Description '.$orederByDescription.'';
    //$query = 'SELECT * FROM owner, pet WHERE pet.ownerid=owner.ownerid AND pet.ownerid="' . $whichOwner . '"';
-   $result=mysqli_query($connection,$query1);
-    if (!$result) {
-         die("database query1 failed.");
+   $result1=mysqli_query($connection,$query1);
+    if (!$result1) {
+         die("database query2 failed.");
      }
     while ($row=mysqli_fetch_assoc($result)) {
         echo $row["ProductID"]. " " .$row["Description"]. " " .$row["Cost"]. " " .$row["Quantity"]. " ";
         echo "<br>";
      }
-     mysqli_free_result($result);
+     mysqli_free_result($result1);
 
 ?>
 </ol>
