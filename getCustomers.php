93505b0d1191ac985a9c185b7b46edc1f8e842f6
JacobHuang <jacobhuang660@gmail.com>
2018-12-03 01:18:44 -0500

    asdd

--- getCustomers.php
+++ getCustomers.php
@@ -1,17 +1,24 @@
-<<?php
-$query = "SELECT * FROM Customers ORDER BY LastName ASC;";
-$result = mysqli_query($connection,$query);
-if (!$result) {
-   die("databases query failed.");
-}
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $query = "SELECT * FROM Customers ORDER BY LastName ASC;";
+    $result = mysqli_query($connection,$query);
+    if (!$result) {
+       die("databases query failed.");
+    }
 
-while ($row = mysqli_fetch_assoc($result)) {
-  echo "<input type='radio' name='Customer' value='";
-  echo $row["CustomerID"]."'>";
-  echo $row["CustomerID"]. " " .$row["LastName"]. " " .$row["FirstName"]. " " .$row["City"]. " ".$row["PhoneNum"]. " " .$row["AgentID"]. " ";
-  echo "<br>";
-}
-mysqli_free_result($result);
-
-
-?>
+    while ($row = mysqli_fetch_assoc($result)) {
+      echo "<input type='radio' name='Customer' value='";
+      echo $row["CustomerID"]."'>";
+      echo $row["CustomerID"]. " " .$row["LastName"]. " " .$row["FirstName"]. " " .$row["City"]. " ".$row["PhoneNum"]. " " .$row["AgentID"]. " ";
+      echo "<br>";
+    }
+    mysqli_free_result($result);
+    ?>
+  </body>
+</html>
--- getdescription.php
+++ getdescription.php
@@ -1,17 +1,26 @@
-<?php
-$whichCus = $_POST["Customer"]; //get selected museum value from the form
-echo $whichCus;
-//$query = "SELECT description FROM Products WHERE ProductID IN (SELECT ProductID FROM Purchase WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE LastName = ".$whichCus."));"; //fill in with correct query
-$query = $query = "SELECT * FROM products, purchases, customers WHERE products.product_id=purchases.product_id AND customers.customer_id = purchases.customer_id AND customers.customer_id = $whichCus";
-echo $query;
-$result = mysqli_query($connection, $query);
- if (!$result) {
- die("databases query on art pieces failed. ");
- }
- echo "<ol>"; //put the artwork in an unordered bulleted list
- while ($row = mysqli_fetch_assoc($result)) {
-   echo "<li>" .$row["description"]. "</li>";
- }
- echo "</ol>"; //end the bulleted list
- mysqli_free_result($result);
- ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $whichCus = $_POST["Customer"]; //get selected museum value from the form
+    echo $whichCus;
+    //$query = "SELECT description FROM Products WHERE ProductID IN (SELECT ProductID FROM Purchase WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE LastName = ".$whichCus."));"; //fill in with correct query
+    $query = $query = "SELECT * FROM products, purchases, customers WHERE products.product_id=purchases.product_id AND customers.customer_id = purchases.customer_id AND customers.customer_id = $whichCus";
+    echo $query;
+    $result = mysqli_query($connection, $query);
+     if (!$result) {
+     die("databases query on art pieces failed. ");
+     }
+     echo "<ol>"; //put the artwork in an unordered bulleted list
+     while ($row = mysqli_fetch_assoc($result)) {
+       echo "<li>" .$row["description"]. "</li>";
+     }
+     echo "</ol>"; //end the bulleted list
+     mysqli_free_result($result);
+     ?>
+  </body>
+</html>
