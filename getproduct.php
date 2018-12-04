9d4303f607505ca7e28051f5db5bf1740db75f48
JacobHuang <jacobhuang660@gmail.com>
2018-12-04 09:59:02 -0500

    asdd

--- getproduct.php
+++ getproduct.php
@@ -1,13 +1,15 @@
 
   <?php
-    $query = "SELECT Description FROM Products;";
+    $query = "SELECT description FROM Products;";
+    echo $query;
     $result = mysqli_query($connection,$query);
     if (!$result) {
        die("databases query failed.");
     }
 
     while ($row = mysqli_fetch_assoc($result)) {
-      echo "<input type='radio' name='Customers' value='";
+      echo $row["description"];
+      echo "<input type='radio' name='description' value='";
       echo $row["description"]."'>";
       echo $row["description"];
       echo "<br>";
