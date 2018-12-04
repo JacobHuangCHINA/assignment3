492ee785ac9d5c63cfc5b7a4104d314e00f3ba3a
JacobHuang <jacobhuang660@gmail.com>
2018-12-04 13:22:30 -0500

    asdd

--- getMaxQuantity.php
+++ getMaxQuantity.php
@@ -4,11 +4,13 @@ $result = mysqli_query($connection,$query);
 if (!$result) {
    die("databases query failed.");
 }
-
-while ($row = mysqli_fetch_assoc($result)) {
-   echo "<input name='quantity' type='number' value='";
-   echo $row["Quantity"]."'>";
-}
+echo ".$maxQuantity.";
+echo "<input name='quantity' type='number' value='";
+echo $row["Quantity"]."'>";
+// while ($row = mysqli_fetch_assoc($result)) {
+//    echo "<input name='quantity' type='number' value='";
+//    echo $row["Quantity"]."'>";
+// }
 mysqli_free_result($result);
 
  ?>
