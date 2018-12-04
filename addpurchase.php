6edbeeaaa922eb3d51c835567c6a031c9850ea27
JacobHuang <jacobhuang660@gmail.com>
2018-12-04 13:49:18 -0500

    asdd

--- addpurchase.php
+++ addpurchase.php
@@ -17,17 +17,16 @@ include 'connectdb.php';
    echo $productDescription;
    echo "----";
    echo $quantity;
-   $query = 'SELECT description FROM Products, Purchase, Customers WHERE Products.ProductID=Purchase.ProductID AND Customers.customerID = Purchase.CustomerID AND Customers.CustomerID = "' . $whichOwner . '"';
    //$query = 'SELECT * FROM owner, pet WHERE pet.ownerid=owner.ownerid AND pet.ownerid="' . $whichOwner . '"';
-   $result=mysqli_query($connection,$query);
-    if (!$result) {
-         die("database query2 failed.");
-     }
-    while ($row=mysqli_fetch_assoc($result)) {
-        echo '<li>';
-        echo $row["description"];
-     }
-     mysqli_free_result($result);
+   // $result=mysqli_query($connection,$query);
+   //  if (!$result) {
+   //       die("database query2 failed.");
+   //   }
+   //  while ($row=mysqli_fetch_assoc($result)) {
+   //      echo '<li>';
+   //      echo $row["description"];
+   //   }
+   //   mysqli_free_result($result);
 ?>
 </ol>
 <?php
