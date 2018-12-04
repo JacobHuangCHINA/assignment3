194b2ab46c9ec615c413e6d9f6ed046f28fa5e45
JacobHuang <jacobhuang660@gmail.com>
2018-12-04 13:59:37 -0500

    asdd

--- addpurchase.php
+++ addpurchase.php
@@ -7,21 +7,27 @@
 <?php
 include 'connectdb.php';
 ?>
-<h1>asdfasdf</h1>
+<h1>Here is the info about added Purchase</h1>
 <?php
    $customerId= $_POST["Customers"];
    $productDescription= $_POST["description"];
    $quantity= $_POST["quantity"];
-   echo "asdf";
+   echo "Customer ID:<br>";
    echo $customerId;
+   echo "<br>";
+   echo "Product description:<br>";
    echo $productDescription;
-   echo "----";
+   echo "<br>";
+   echo "Number of item: <br>";
    echo $quantity;
-   //$query = 'SELECT * FROM owner, pet WHERE pet.ownerid=owner.ownerid AND pet.ownerid="' . $whichOwner . '"';
-   // $result=mysqli_query($connection,$query);
-   //  if (!$result) {
-   //       die("database query2 failed.");
-   //   }
+   echo "<br>";
+   $query = 'SELECT MAX(Quantity) FROM Purchase WHERE CustomerID = 14 AND ProductID = 66;';
+   $result=mysqli_query($connection,$query);
+    if (!$result) {
+         die("database query2 failed.");
+     }
+     $row=mysqli_fetch_assoc($result)
+     echo $row;
    //  while ($row=mysqli_fetch_assoc($result)) {
    //      echo '<li>';
    //      echo $row["description"];
