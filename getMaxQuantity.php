4ef491a7a55a6fc02eae77e0111d71fed632d54c
JacobHuang <jacobhuang660@gmail.com>
2018-12-04 13:30:06 -0500

    asdd

--- getMaxQuantity.php
+++ getMaxQuantity.php
@@ -1,5 +1,10 @@
 <?php
-$maxQuantity = "SELECT MAX(Quantity) FROM Purchase;";
+$customerId = $_POST["Customers"];
+$productDescription= $_POST["description"];
+$maxQuantity = 'SELECT MAX(Quantity) FROM Purchase WHERE CustomerID = '.$customerId.' AND ProductID = '.$productDescription.'';
+echo $customerId;
+echo $productDescription;
+echo $maxQuantity;
 $result = mysqli_query($connection,$query);
 if (!$result) {
    die("databases query failed.");
