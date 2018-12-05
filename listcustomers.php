787b619efd9bb65368abe9707defd53e54a97be1
JacobHuang <jacobhuang660@gmail.com>
2018-12-05 08:58:05 -0500

    asdf

--- listcustomers.php
+++ listcustomers.php
@@ -6,7 +6,19 @@
   </head>
   <body>
     <ol>
-      <li>hoeel</li>
+      <?php
+      $quantity= $_POST["quantity"];
+      $query = 'SELECT LastName FROM Customers' ;
+      $result=mysqli_query($connection,$query);
+       if (!$result) {
+            die("database query2 failed.");
+        }
+       while ($row=mysqli_fetch_assoc($result)) {
+           echo '<li>';
+           echo $row["LastName"];
+        }
+        mysqli_free_result($result);
+       ?>
     </ol>
   </body>
 </html>
