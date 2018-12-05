a1f23930147105f4c8737e19a11e9ce032237354
JacobHuang <jacobhuang660@gmail.com>
2018-12-05 00:03:40 -0500

    asdf

--- home.php
+++ home.php
@@ -78,7 +78,7 @@ include 'getCustomers.php';
 <h4>Q7: List Customers: </h4>
 <form action="listcustomers.php" method="post">
   Quantity of product:
-  <input type="number" name="number" min="0">
+  <input name='quantity' type='number' min="0">
   <input type="submit">
 </form>
 <hr>
