06bab810b8db20eb88e1e17af484469ce3e8d311
JacobHuang <jacobhuang660@gmail.com>
2018-12-04 23:36:40 -0500

    asdf

--- listcustomers.php
+++ listcustomers.php
@@ -1,18 +1,27 @@
-<?php
-$quantity = $_Post["numofitem"];
-echo $quantity;
-$query = 'SELECT Customers.FirstName,Customers.LastName,Purchase.CustomerID,Purchase.ProductID,Products.Description FROM Purchase,Products,Customers WHERE Purchase.Quantity > '.$quantity.' AND Purchase.ProductID = Products.ProductID AND Customers.CustomerID = Purchase.CustomerID;';
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ol>
+      <?php
+      $quantity = $_Post["numofitem"];
+      echo $quantity;
+      $query = 'SELECT Customers.FirstName,Customers.LastName,Purchase.CustomerID,Purchase.ProductID,Products.Description FROM Purchase,Products,Customers WHERE Purchase.Quantity > '.$quantity.' AND Purchase.ProductID = Products.ProductID AND Customers.CustomerID = Purchase.CustomerID;';
 
-$result = mysqli_query($connection,$query);
-if (!$result) {
-   die("databases query failed.");
-}
-<ol>
-while ($row = mysqli_fetch_assoc($result)) {
-  echo "<li>";
-  echo $row["FirstName"]. " " .$row["LastName"]. " " .$row["Description"]. " ";
-  echo "</li>";
-}
-</ol>
-mysqli_free_result($result);
- ?>
+      $result = mysqli_query($connection,$query);
+      if (!$result) {
+         die("databases query failed.");
+      }
+      while ($row = mysqli_fetch_assoc($result)) {
+        echo "<li>";
+        echo $row["FirstName"]. " " .$row["LastName"]. " " .$row["Description"]. " ";
+        
+      }
+      mysqli_free_result($result);
+       ?>
+    </ol>
+  </body>
+</html>
