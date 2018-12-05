422a8fab91f1b15837ed5e1b8e479bc9ca4c6dac
JacobHuang <jacobhuang660@gmail.com>
2018-12-04 23:40:02 -0500

    asdf

--- listcustomers.php
+++ listcustomers.php
@@ -9,7 +9,7 @@
       <?php
       $quantity = $_Post["numofitem"];
       echo $quantity;
-      $query = 'SELECT Customers.FirstName,Customers.LastName,Purchase.CustomerID,Purchase.ProductID,Products.Description FROM Purchase,Products,Customers WHERE Purchase.Quantity > '.$quantity.' AND Purchase.ProductID = Products.ProductID AND Customers.CustomerID = Purchase.CustomerID;';
+      $query = 'SELECT Customers.FirstName as FistName,Customers.LastName as LastName,Purchase.CustomerID,Purchase.ProductID,Products.Description as Description FROM Purchase,Products,Customers WHERE Purchase.Quantity > '.$quantity.' AND Purchase.ProductID = Products.ProductID AND Customers.CustomerID = Purchase.CustomerID;';
 
       $result = mysqli_query($connection,$query);
       if (!$result) {
@@ -18,7 +18,7 @@
       while ($row = mysqli_fetch_assoc($result)) {
         echo "<li>";
         echo $row["FirstName"]. " " .$row["LastName"]. " " .$row["Description"]. " ";
-        
+
       }
       mysqli_free_result($result);
        ?>
