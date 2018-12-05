770de12ee92db3e26da8f6cc4bc1fbbf72f61544
JacobHuang <jacobhuang660@gmail.com>
2018-12-04 20:46:18 -0500

    aad

--- insertcustomer.php
+++ insertcustomer.php
@@ -33,7 +33,7 @@ echo $angentId;
    $newangentId = (string)$angentId;
    echo $newangentId;
   // get min purchase
-   $query1 = 'INSERT INTO Customers VALUES ("'.$customerId.' "," '.$firstName.' "," '. $lastName.' "," '.$city.' ","  '.$phoneNumber.' "," '.$newangentId.'")';
+   $query1 = 'INSERT INTO Customers (CustomerID, FirstName, LastName, City, PhoneNum, AgentID) VALUES ("'.$customerId.' "," '.$firstName.' "," '. $lastName.' "," '.$city.' ","  '.$phoneNumber.' "," '.$newangentId.'")';
 //$query2 = 'INSERT INTO Purchase VALUES ("'.$customerId.' "," '. $productID.' "," '.$quantity.'")';
    if (!mysqli_query($connection,$query1)) {
      die("Error: insert failed" . mysqli_error($connection));
