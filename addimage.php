e7ae45ce72f47f253122f3404101104f17c90871
JacobHuang <jacobhuang660@gmail.com>
2018-12-05 19:03:49 -0500

    ASDF

--- addimage.php
+++ addimage.php
@@ -13,7 +13,7 @@ include 'connectdb.php';
    $whichCustomer = intval($_POST["Customers"]);
    $row=mysqli_fetch_assoc($result);
 
-   $query = 'INSERT INTO Customers (cusimage) values("'.$pic.'") WHERE CustomerID = "'.$whichCustomer.'";' ;
+   $query = 'INSERT INTO Customers (cusimage) values('.$pic.') WHERE CustomerID = "'.$whichCustomer.'";' ;
    echo $query;
    if (!mysqli_query($connection, $query)) {
         die("Error: insert failed" . mysqli_error($connection));
